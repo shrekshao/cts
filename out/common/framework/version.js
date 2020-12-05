8a7528596b5636747984f680ebdd12bef6198112
kainino0x <kainino1@gmail.com>
2020-12-05 03:09:20 +0000

    Deploying to gh-pages from  @ bddeaea12f545e9c6011b9bbc971e1ade21b3059 🚀

--- out/common/framework/version.js
+++
@@ -1,3 +0,0 @@
-// AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
-
-export const version = '20bbb0b50d1d09cce172641fc512ddc4f23260ce';
--- out/unittests/unit_test.js
+++
@@ -1,4 +0,0 @@
-/**
-* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
-**/import { Fixture } from '../common/framework/fixture.js';export class UnitTest extends Fixture {}
-//# sourceMappingURL=unit_test.js.map
