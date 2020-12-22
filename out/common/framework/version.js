694d68c02f47dbe1b4793fba58f8255b3be31469
kainino0x <kainino1@gmail.com>
2020-12-22 03:19:58 +0000

    Deploying to gh-pages from  @ 672c25a7c11e49d7adf3fe38e832efd6f58c0a83 🚀

--- out/common/framework/version.js
+++ out/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '6c4d36b901ca642cb474120efe23e9d1bc746fa1';
+export const version = '672c25a7c11e49d7adf3fe38e832efd6f58c0a83';
